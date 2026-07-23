5c81507f5800b2f07b01fc6b5ed9a4fcf27447c3
howIdobetter <2079153913@qq.com>
2026-07-23 10:33:42 +0800

    248

--- 124.二叉树中的最大路径和.cpp
+++ 124.二叉树中的最大路径和.cpp
@@ -19,7 +19,16 @@
 class Solution {
 public:
     int maxPathSum(TreeNode* root) {
-        
+        int maxResult = INT_MIN;
+        dfs(root, maxResult);
+        return maxResult;
+    }
+    int dfs(TreeNode* node, int& maxResult) {
+        if (!node) return 0;
+        int left = max(0, dfs(node->left, maxResult));
+        int right = max(0, dfs(node->right, maxResult));
+        maxResult = max(maxResult, left + node->val + right);
+        return node->val + max(left, right);
     }
 };
 // @lc code=end
